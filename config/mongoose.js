430ce2f5d9dea5d4ae085c3a0f1014eab4138c71
satviksaxena <satviksaxena1@gmail.com>
2021-07-06 10:27:39 +0530

    db changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/task_list_db');
+mongoose.connect('mongodb://localhost/task_list_db', { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'error connecting to db'));
-db.once('open', function(){
+db.once('open', function() {
     console.log('Successfully connected to database');
 });
