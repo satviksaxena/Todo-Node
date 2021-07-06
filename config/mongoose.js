03063546ca95b995c0006cfb388c0d4474c0a0f4
satviksaxena <satviksaxena1@gmail.com>
2021-07-06 10:56:56 +0530

    mongoose changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/task_list_db', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/task_list_db', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'error connecting to db'));
